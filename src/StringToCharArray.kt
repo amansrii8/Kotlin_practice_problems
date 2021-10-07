6a8ef9373b42a6886c57a2186e405f2fc1c555df
aman.s <aman.s@ethinos.com>
2021-10-07 21:55:18 +0530

    # Example: 1. Convert String to char array

---
+++ src/StringToCharArray.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+/*
+    1. Convert String to char array
+ */
+
+fun main() {
+
+    val s = "Today's, whether is awesome"
+
+    val chars = s.toCharArray()
+
+    println(chars.contentToString())
+}
