4b653662986015289fd039020ad8c1512d090382
aman.s <aman.s@ethinos.com>
2021-10-07 21:48:27 +0530

    Initial commit

---
+++ src/CharArrayToString.kt
@@ -0,0 +1,16 @@
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+/*
+     1. Convert char array to String
+ */
+
+fun main() {
+
+    val ch = charArrayOf('a', 'e', 'i', 'o', 'u')
+
+    print(String(ch))
+
+}
+
+
