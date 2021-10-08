d7d4f1514b6970831d085e9c2b3b6dbb00cf87bb
aman.s <aman.s@ethinos.com>
2021-10-08 18:06:41 +0530

    # Example: 1. Print an Array using standard library Arrays

---
+++ src/PrintAnArrayWithSLF.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+/*
+    1. Print an Array using standard library Arrays
+ */
+
+fun main() {
+    val array = intArrayOf(1,2,3,4,5,6)
+
+    println(array.contentToString())
+}
