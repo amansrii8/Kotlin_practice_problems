a82b11d5ce6e49eb57ac73b33e7d44de5b69fb3f
aman.s <aman.s@ethinos.com>
2021-10-08 18:49:07 +0530

    # Example: 1. Convert array to array list

---
+++ src/ConvertArrayToArrayList.kt
@@ -0,0 +1,14 @@
+
+/*
+    1. Convert array to array list
+ */
+
+fun main() {
+
+    val array = arrayOf("a", "e", "i", "o", "u")
+
+    println(array.contentToString())
+
+    println(array.toList())
+
+}
