c2a2542a73ebf31608ca53bfba794dd2e301afd8
aman.s <aman.s@ethinos.com>
2021-10-14 12:52:49 +0530

    # Example: 1. Sort a Map By Values

---
+++ src/SortAMapByValues.kt
@@ -0,0 +1,12 @@
+
+/*
+    1. Sort a Map By Values
+ */
+
+fun main() {
+
+    val map = mapOf(1 to "Alok", 2 to "Bad boy", 7 to "Avinash", 4 to "Deepak")
+
+
+    println(map.toSortedMap().values)
+}
